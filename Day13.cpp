3314eb5ad8c859369d28b24fd5fbcbb39adcb31b
Tristan Carter <tristan.carter@blueskydevops.net>
2021-12-20 09:09:42 -0500

    Day 13

--- Day13.cpp
+++ Day13.cpp
@@ -11,7 +11,7 @@
 using namespace std;
 int day13::part1()
 {
-    ifstream infile("../test13.txt");
+    ifstream infile("../day13.txt");
     string line;
     bool isInstructions = false;
     paper.push_back(vector<char>(1, '.'));
@@ -30,7 +30,7 @@ int day13::part1()
             getline(ss, axis, '=');
             ss >> num;
             paper = FoldPaper(num, axis.back());
-            //break;
+            break;
         }
         else
         {
@@ -73,6 +73,69 @@ int day13::part1()
 
 int day13::part2()
 {
+    ifstream infile("../day13.txt");
+    string line;
+    bool isInstructions = false;
+    paper.push_back(vector<char>(1, '.'));
+    while (getline(infile, line))
+    {
+        if (line == "")
+        {
+            isInstructions = true;
+            continue;
+        }
+        else if (isInstructions)
+        {
+            stringstream ss(line);
+            string axis;
+            int num;
+            getline(ss, axis, '=');
+            ss >> num;
+            paper = FoldPaper(num, axis.back());
+        }
+        else
+        {
+            stringstream ss(line);
+            string tmp;
+            int x, y;
+            getline(ss, tmp, ',');
+            y = stoi(tmp);
+            ss >> x;
+
+            if (x >= paper.size())
+            {
+                vector<char> v(paper[0].size(), '.');
+                paper.resize(x + 1, v);
+            }
+            if (y >= paper.back().size())
+            {
+                for (int i = 0; i < paper.size(); i++)
+                {
+                    paper[i].resize(y + 1, '.');
+                }
+            }
+
+            paper[x][y] = '#';
+        }
+    }
+    for (int i = 0; i < paper.size(); i++)
+    {
+        for (int j = 0; j < paper[i].size(); j++)
+            cout << paper[i][j] << "";
+        cout << "\n";
+    }
+    cout << "\n";
+
+    int dots = 0;
+    for (int i = 0; i < paper.size(); i++)
+    {
+        for (int j = 0; j < paper[i].size(); j++)
+            if (paper[i][j] == '#')
+                dots++;
+    }
+
+    printf("\nThe total dots are: %i\n", dots);
+
     return 1;
 }
 
@@ -86,14 +149,14 @@ vector< vector<char> > day13::FoldPaper(int num, char axis)
         foldedPaper.resize(num, tmp);
 
         int it = foldedPaper.size() - 1;
-        for (int i = paper.size()/2 + 1; i < paper.size() && it >= 0; i++, it--)
+        for (int i = num + 1; i < paper.size() && it >= 0; i++, it--)
         {
             for (int j = 0; j < paper[i].size(); j++)
             {
                 foldedPaper[it][j] = paper[i][j];
             }
         }
-        for (int i = foldedPaper.size() - num; i < foldedPaper.size(); i++)
+        for (int i = 0; i < foldedPaper.size(); i++)
         {
             for (int j = 0; j < foldedPaper[i].size(); j++)
             {
@@ -101,14 +164,6 @@ vector< vector<char> > day13::FoldPaper(int num, char axis)
                     foldedPaper[i][j] = '#';
             }
         }
-
-        for (int i = 0; i < foldedPaper.size(); i++)
-        {
-            for (int j = 0; j < foldedPaper[i].size(); j++)
-                cout << foldedPaper[i][j] << "";
-            cout << "\n";
-        }
-        cout << "\n";
     }
     else if (axis == 'x')
     {
@@ -116,30 +171,32 @@ vector< vector<char> > day13::FoldPaper(int num, char axis)
         foldedPaper.resize(paper.size(), tmp);
 
         int it = foldedPaper[0].size() - 1;
-        for (int i = paper[0].size() - num; i < paper[0].size() && it >= 0; i++, it--)
+        for (int i = num + 1; i < paper[0].size() && it >= 0; i++, it--)
         {
-            for (int j = 0; j < foldedPaper[0].size(); j++)
+            for (int j = 0; j < foldedPaper.size(); j++)
             {
                 foldedPaper[j][it] = paper[j][i];
             }
         }
-        for (int i = foldedPaper.size() - num; i < foldedPaper.size(); i++)
-        { // Something wrong here
+        for (int i = 0; i < foldedPaper.size(); i++)
+        { 
             for (int j = 0; j < foldedPaper[i].size(); j++)
             {
                 if (paper[i][j] == '#')
                     foldedPaper[i][j] = '#';
             }
         }
+    }
 
-        for (int i = 0; i < foldedPaper.size(); i++)
+    return foldedPaper;
+}
+
+/* Print the output
+for (int i = 0; i < foldedPaper.size(); i++)
         {
             for (int j = 0; j < foldedPaper[i].size(); j++)
                 cout << foldedPaper[i][j] << "";
             cout << "\n";
         }
         cout << "\n";
-    }
-
-    return foldedPaper;
-}
+ */
